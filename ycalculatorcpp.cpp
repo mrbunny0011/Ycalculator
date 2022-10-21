7c783dc2e501d96c66fad3c994c4c6f16d1bf3f5
mrbunny0011 <116324488+mrbunny0011@users.noreply.github.com>
2022-10-21 09:36:35 +0500

    Add files via upload

---
+++ ycalculatorcpp.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a;
+    int b;
+    char op;
+    bool loop = 1;
+    while(loop=1)
+    {
+      cout<<"Enter your first number"<<endl;
+    cin>>a;
+      cout<<"Enter your second number"<<endl;
+    cin>>b;
+      cout<<"Enter your operater(+,-,*,/,%)"<<endl;
+    cin>>op;
+    
+    if(op=='+')
+    cout<<a<<'+'<<b<<'='<<a+b<<endl;
+    else
+    if(op=='-')
+    cout<<a<<'-'<<b<<'='<<a-b<<endl;
+    else
+    if(op=='*')
+    cout<<a<<'*'<<b<<'='<<a*b<<endl;
+    else 
+    if(op=='/')
+    cout<<a<<'/'<<b<<'='<<a/b<<endl;
+    else 
+    if(op='%')
+    cout<<a<<'%'<<b<<'='<<a%b<<endl;
+    else
+     if (a,b==char())
+    cout<<" wrong input";
+    else 
+    loop = 0;
+    
+      }
+   cout <<"invalid input";
+    
+    return 0;
+}
